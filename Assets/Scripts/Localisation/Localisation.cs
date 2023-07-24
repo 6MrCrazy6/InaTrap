ecd9de5f141d14c66c0ac906ff4bfef5c96cd318
Inna Varchenko <117535858+6MrCrazy6@users.noreply.github.com>
2023-07-24 22:28:26 +0300

    Delete Assets/Scripts directory

--- Assets/Scripts/Localisation/Localisation.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Localisation
-{
-    public static Localisation Inst = new Localisation();
-    public string langs = "en";
-}
